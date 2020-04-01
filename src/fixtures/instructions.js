bb9a7635ca31d5908ecdab1cd9493ee404ad4b6f
Rasmus Ståhl <rasmus.stahl.47@gmail.com>
2020-04-01 19:42:40 +0200

    added scrolling in modal

--- src/fixtures/instructions.js
+++ src/fixtures/instructions.js
@@ -1,5 +1,43 @@
 
+// export default 'This game can be played with two or more people. \
+//                 One person will act as game leader. This person selects a story and reads its premise \
+//                 alound to everyone (click one of the icons on the Stories page). \
+//                 The game leader then reads the backstory to him-/herself without revealing it to the group \
+//                 (click the button labeled Backstory). \
+//                 The rest of the group then have to ask questions to the game leader \
+//                 to figure out what the backstory is. \
+//                 They may only ask questions with the answer \'yes\' or \'no\'. \
+//                 The game leader may reply to questions where incorrect assumptions have been made \
+//                 with: \'You\'ve made an incorrect assumption\'. \
+//                 If someone in the group are too scared to play, then please go easy on them. \
+//                 Just take a deep breath and try to understand that everyone may not have been blessed with \
+//                 your level of coolness.'
+
 export default 'This game can be played with two or more people. \
+                One person will act as game leader. This person selects a story and reads its premise \
+                alound to everyone (click one of the icons on the Stories page). \
+                The game leader then reads the backstory to him-/herself without revealing it to the group \
+                (click the button labeled Backstory). \
+                The rest of the group then have to ask questions to the game leader \
+                to figure out what the backstory is. \
+                They may only ask questions with the answer \'yes\' or \'no\'. \
+                The game leader may reply to questions where incorrect assumptions have been made \
+                with: \'You\'ve made an incorrect assumption\'. \
+                If someone in the group are too scared to play, then please go easy on them. \
+                Just take a deep breath and try to understand that everyone may not have been blessed with \
+                your level of coolness. This game can be played with two or more people. \
+                One person will act as game leader. This person selects a story and reads its premise \
+                alound to everyone (click one of the icons on the Stories page). \
+                The game leader then reads the backstory to him-/herself without revealing it to the group \
+                (click the button labeled Backstory). \
+                The rest of the group then have to ask questions to the game leader \
+                to figure out what the backstory is. \
+                They may only ask questions with the answer \'yes\' or \'no\'. \
+                The game leader may reply to questions where incorrect assumptions have been made \
+                with: \'You\'ve made an incorrect assumption\'. \
+                If someone in the group are too scared to play, then please go easy on them. \
+                Just take a deep breath and try to understand that everyone may not have been blessed with \
+                your level of coolness. This game can be played with two or more people. \
                 One person will act as game leader. This person selects a story and reads its premise \
                 alound to everyone (click one of the icons on the Stories page). \
                 The game leader then reads the backstory to him-/herself without revealing it to the group \
